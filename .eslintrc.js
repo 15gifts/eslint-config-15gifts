555dcaa10f6de081320307d45d4c1ea30b509b8e
Dale Blackburn <dale@dakebl.co.uk>
2019-01-31 15:00:04 +0000

    initial commit of base without rules

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    extends: [
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'standard'
+    ],
+    parser: 'babel-eslint',
+    env: {
+      es6: true,
+      browser: true,
+      mocha: true,
+      node: true,
+      jest: true
+    },
+    settings: {
+        react: {
+            version: '16.0'
+        },
+        'import/resolver': {
+            node: {
+            paths: ['./src']
+            }
+        }
+    },
+    plugins: [
+      'react',
+      'jsdoc'
+    ],
+    rules: {}
+  }
+  
