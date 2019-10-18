2302f6d445d5964028bd5913bddd11fe8bf2904d
Dean Pugh <deanpugh@deanpugh.com>
2019-10-18 10:29:43 +0100

    Make error instead of warning

--- .eslintrc.js
+++ .eslintrc.js
@@ -34,7 +34,7 @@ module.exports = {
   ],
   rules: {
     'node/no-deprecated-api': 1,
-    'comma-dangle' : [1, 'always-multiline'],
+    'comma-dangle' : [2, 'always-multiline'],
     'curly': [2, 'multi-line', 'consistent'],
     'react-hooks/rules-of-hooks': 'error',
     'no-unused-vars': [2, { 'ignoreRestSiblings': false }]
